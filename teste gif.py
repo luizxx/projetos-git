c1354cb7e6746414e256b7ff14f5731951d3080f
luizxx <luizotavio308@gmail.com>
2021-04-11 17:13:02 -0300

    primeiro commit

---
+++ teste gif.py
@@ -0,0 +1,79 @@
+
+p  = float (input('Digite o seu peso, jogador 1.'))
+p1 = float (input('Digite o seu peso, jogador 2.'))
+p2 = float (input('Digite o seu peso, jogador 3.'))
+p3 = float (input('Digite o seu peso, jogador 4.'))
+p4 = float (input('Digite o seu peso, jogador 5.'))
+
+if p > p1 and p > p2 and p > p3 and p > p4 :
+    print(f' {p} é o maior peso. [jogador 1 ]')
+    if p1 < p2 and p1 < p3 and p1 < p4 and p1 < p :
+        print (f'{p1} é o menor peso [jogador 2] ')
+    elif p2 < p3 and p2 < p4 and p2 < p1 and p2 < p :
+        print(f'{p2} é o menor peso [jogador 3 ]')
+    elif p3 < p4 and p3 < p1 and p3 < p2 and p3 < p :
+        print(f'{p3} é o menor peso [jogador 4 ]')
+    elif p4 < p1 and p4 < p2 and p4 < p3 and p4 < p :
+        print(f'{p4} é o menor peso  peso [jogador 5 ]')
+elif p1 > p2 and p1 > p3 and p1 > p4 and p1 > p :
+    print (f'{p1} é o maior peso [jogador 2] ')
+    if p < p1 and p < p2 and p < p3 and p < p4 :
+         print(f' {p} é o menor peso. [jogador 1 ]')
+    elif p1 < p2 and p1 < p3 and p1 < p4 and p1 < p :
+         print (f'{p1} é o menor peso [jogador 2] ')    
+    elif p2 < p3 and p2 < p4 and p2 < p1 and p2 < p :
+         print(f'{p2} é o menor peso [jogador 3 ]')
+    elif p3 < p4 and p3 < p1 and p3 < p2 and p3 < p :
+         print(f'{p3} é o menor peso [jogador 4 ]')
+    elif p4 < p1 and p4 < p2 and p4 < p3 and p4 < p :
+         print(f'{p4} é o menor peso  peso [jogador 5 ]')
+elif p2 > p3 and p2 > p4 and p2 > p1 and p2 > p :
+    print(f'{p2} é o maior peso [jogador 3 ]')
+    if p < p1 and p < p2 and p < p3 and p < p4 :
+         print(f' {p} é o menor peso. [jogador 1 ]')
+    elif p1 < p2 and p1 < p3 and p1 < p4 and p1 < p :
+         print (f'{p1} é o menor peso [jogador 2] ')    
+    elif p2 < p3 and p2 < p4 and p2 < p1 and p2 < p :
+         print(f'{p2} é o menor peso [jogador 3 ]')
+    elif p3 < p4 and p3 < p1 and p3 < p2 and p3 < p :
+         print(f'{p3} é o menor peso [jogador 4 ]')
+    elif p4 < p1 and p4 < p2 and p4 < p3 and p4 < p :
+         print(f'{p4} é o menor peso  peso [jogador 5 ]')
+elif p3 > p4 and p3 > p1 and p3 > p2 and p3 > p :
+    print(f'{p3} é o maior peso [jogador 4 ]')
+    if p < p1 and p < p2 and p < p3 and p < p4 :
+         print(f' {p} é o menor peso. [jogador 1 ]')
+    elif p1 < p2 and p1 < p3 and p1 < p4 and p1 < p :
+         print (f'{p1} é o menor peso [jogador 2] ')    
+    elif p2 < p3 and p2 < p4 and p2 < p1 and p2 < p :
+         print(f'{p2} é o menor peso [jogador 3 ]')
+    elif p3 < p4 and p3 < p1 and p3 < p2 and p3 < p :
+         print(f'{p3} é o menor peso [jogador 4 ]')
+    elif p4 < p1 and p4 < p2 and p4 < p3 and p4 < p :
+         print(f'{p4} é o menor peso  peso [jogador 5 ]')
+elif p4 > p1 and p4 > p2 and p4 > p3 and p4 > p :
+    print(f'{p4} é o maior peso [jogador 5 ]')
+    if p < p1 and p < p2 and p < p3 and p < p4 :
+         print(f' {p} é o menor peso. [jogador 1 ]')
+    elif p1 < p2 and p1 < p3 and p1 < p4 and p1 < p :
+         print (f'{p1} é o menor peso [jogador 2] ')    
+    elif p2 < p3 and p2 < p4 and p2 < p1 and p2 < p :
+         print(f'{p2} é o menor peso [jogador 3 ]')
+    elif p3 < p4 and p3 < p1 and p3 < p2 and p3 < p :
+         print(f'{p3} é o menor peso [jogador 4 ]')
+    elif p4 < p1 and p4 < p2 and p4 < p3 and p4 < p :
+         print(f'{p4} é o menor peso  peso [jogador 5 ]')
+elif p == p1 and p == p2 and p == p3 and p == p4 :
+    print('são TODOS iguais.')
+
+
+else: print('error')
+print('luiz é foda')
+
+
+
+
+
+
+
+
